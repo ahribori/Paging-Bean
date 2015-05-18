830b419dc78b2d8710e1e2ebe4a1daeef6ad1067
hs9923 <hs9923@naver.com>
2015-05-18 09:30:12 +0900

    encode utf-8로 변경

--- src/test/TestPaging.java
+++ src/test/TestPaging.java
@@ -5,37 +5,35 @@ import model.Paging;
 public class TestPaging {
 	public static void main(String[] args) {
 		
-		
-		System.out.println("HELLO");
 		/*
-		 * Paging Bean ��� ����
+		 * Paging Bean 사용 예제
 		 */
-		Paging p = new Paging(3432); // ���ڿ� ����Ʈ�� �� �� ���� �־ ��ü�� ���ϸ� �����ϰ� ����¡ ó���� �˴ϴ�.
+		Paging p = new Paging(3432); // 생성자에 리스트의 총 행 수를 넣어서 객체를 생성하면 간편하게 페이징 처리가 됩니다.
 		
 		
-		// �� ȭ�鿡 ������ ���� ������ setter �޼ҵ带 ���� ���� �� �� �ֽ��ϴ�. (�̼����� �⺻�� 10) 
-		p.setPageSize(8);
+		// 한 화면에 보여줄 행의 갯수를 setter 메소드를 통해 설정 할 수 있습니다. (미설정시 기본값 10) 
+		//p.setPageSize(5);
 		
 		
-		// �� ������ �׷쿡 ������ �������� ������ setter �޼ҵ带 ���� ���� �� �� �ֽ��ϴ�. (�̼�����  �⺻�� 5)
-		p.setPageGroupSize(3);
+		// 한 페이지 그룹에 보여줄 페이지의 갯수를 setter 메소드를 통해 설정 할 수 있습니다. (미설정시  기본값 5)
+		//p.setPageGroupSize(3);
 		
 		
-		// setter �޼ҵ�� �������� �̵��� �� �ֽ��ϴ�. (�̼�����  �⺻�� 1)
-		p.setCurrentPage(13);
+		// setter 메소드로 페이지를 이동할 수 있습니다. (미설정시  기본값 1)
+		p.setCurrentPage(12);
 		
 		
-		//toString() �������̵� �� ���ҽ��ϴ�.
+		//toString() 오버라이딩 해 놓았습니다.
 		System.out.println(p);
 		
 		
-		//����¡ ó�� �� ����� �̸� �� �� �ֽ��ϴ�.
+		//페이징 처리 된 모습을 미리 볼 수 있습니다.
 		System.out.println(p.preview());
 		
 		
 		/*
-		 *  ������ ���� ���ڷ� �ѹ� ��� ������ �� ���� �ֽ��ϴ�.
-		 *  �ڼ��� ������ Paging Ŭ������ �������ּ���.�ּ� ������ �޾Ƴ��ҽ��ϴ�.
+		 *  다음과 같이 생성자로 한번에 모든 설정을 할 수도 있습니다.
+		 *  자세한 설명은 Paging 클래스를 참조해주세요.주석 열심히 달아놓았습니다.
 		 */
 		
 //		Paging p = new Paging(27, 10, 7, 2424);
